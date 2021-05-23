1a6641ab152992023ca9994e312986f72ec4aee4
15719021 <>
2021-05-23 14:04:07 +0000

    I added lines to create part one's code.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -18,9 +18,9 @@ data = None
 
 # YOUR CODE HERE 1
 fp = r'data/1091402.txt'
-data = pd.read_csv(fp, delim_whitespace=True, na_values=['*', '**', '***', '****', '*****', '******'])
-data.head()
-data.columns
+data = pd.read_csv(fp, delim_whitespace=True, skiprows=[1], na_values=[-9999])
+print(data.head())
+print(data.tail())
 # ### Part 2 
 # 
 # In this section, you will calculate simple statistics based on the input data:
@@ -30,7 +30,7 @@ data.columns
 
 tavg_nodata_count = None
 #YOUR CODE HERE 2
-tavg_nodata_count=str(sum(pd.isnull(fp))
+tavg_nodata_count=sum(pd.isnull(data))
 
 
 #CAUTION!!! DON'T EDIT THIS PART START
